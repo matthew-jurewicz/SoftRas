be56fe17d8f826d663d545342870f5d329d20832
Matthew Jurewicz <matthewjurewicz@Matthews-MacBook-Air.local>
2020-12-05 18:08:15 -0500

    fix OOM

--- examples/draw.py
+++ examples/draw.py
@@ -10,18 +10,18 @@ renderer = sr.SoftRenderer(
 parent = os.path.dirname(os.path.abspath(__file__))
 cam_info = np.load(parent + '/../data/camera.npy')
 cam_info = torch.from_numpy(cam_info.astype(np.float32))
-renderer.transform.set_eyes_from_angles(
-    distances=cam_info[:,0], 
-    elevations=cam_info[:,1], 
-    azimuths=cam_info[:,2]
-)
 object = sr.Mesh.from_obj(file, normalization=True)
 object = sr.Mesh(
-    object.vertices.repeat(len(cam_info), 1, 1), 
-    object.faces.repeat(len(cam_info), 1, 1)
+    object.vertices.repeat(10, 1, 1), 
+    object.faces.repeat(10, 1, 1)
 )
-images = renderer.render_mesh(object) * 255
-np.save(
-    images.detach().cpu().numpy().astype(np.uint8), 
-    parent + '/../data/output.npy'
-)
+images = np.empty((len(cam_info), 4, 64, 64), dtype=np.uint8)
+for i in range(0, len(cam_info), 10):
+    renderer.transform.set_eyes_from_angles(
+        distances=cam_info[i:i + 10,0], 
+        elevations=cam_info[i:i + 10,1], 
+        azimuths=cam_info[i:i + 10,2]
+    )
+    tmp =  renderer.render_mesh(object) * 255
+    images[i:i + 10] = tmp.detach().cpu().numpy().astype(np.uint8)
+np.save(images, parent + '/../data/output.npy')
